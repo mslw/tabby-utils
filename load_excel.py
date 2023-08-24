ce50cde4f4d3d3d24829209a261c7aa00257d2e6
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-08-24 20:35:50 +0200

    Add a script for renaming of tsv files
    This expands load_excel to also take a bunch of prefixed tsv files and
    copy them into a "directory-of-files". This would not be necessary as
    such, but I currently want to do this for two reasons. One is the
    desire for unification, the other (more important) is that context
    machinnery works better in this layout.

--- load_excel.py
+++ load_excel.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from datalad_tabby.io.xlsx import xlsx2tabby
 from pathlib import Path
+from shutil import copy
 
 
 def get_prefix_sheet(fpath):
@@ -34,19 +35,34 @@ def affix_convention(fpath):
 
 
 parser = ArgumentParser()
-parser.add_argument("xslx_file", type=Path, help="source xlsx file")
-parser.add_argument("dest_dir", type=Path, help="directory to deposit tabby files")
+grp = parser.add_mutually_exclusive_group(required=True)
+grp.add_argument("-x", "--xlsx_file", type=Path, help="source xlsx file")
+grp.add_argument("-t", "--tsv_files", type=Path, nargs="*", help="source tsv files")
+parser.add_argument("-d", "--dest_dir", type=Path, required=True, help="directory to deposit tabby files")
 args = parser.parse_args()
 
-res = xlsx2tabby(
-    src=args.xslx_file,
-    dest=args.dest_dir,
-)
+if args.xlsx_file is not None:
+    res = xlsx2tabby(
+        src=args.xslx_file,
+        dest=args.dest_dir,
+    )
+    for fpath in res:
+        newpath = affix_convention(get_dirpath_equivalent(fpath))
+        print(newpath)
+        if newpath != fpath:
+            if not newpath.parent.is_dir():
+                newpath.parent.mkdir()
+            fpath.rename(newpath)
 
-for fpath in res:
-    newpath = affix_convention(get_dirpath_equivalent(fpath))
-    print(newpath)
-    if newpath != fpath:
-        if not newpath.parent.is_dir():
-            newpath.parent.mkdir()
-        fpath.rename(newpath)
+
+if args.tsv_files is not None:
+    for fpath in args.tsv_files:
+        # hypothetical dirpath with convention
+        newpath_here = affix_convention(get_dirpath_equivalent(fpath))
+        # what we actually want
+        newpath_there = args.dest_dir / newpath_here.parent.name / newpath_here.name
+        print(newpath_there)
+        if not newpath_there.parent.is_dir():
+            newpath_there.parent.mkdir()
+        copy(fpath, newpath_there)
+
