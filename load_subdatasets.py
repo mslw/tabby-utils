8055f01c3fddf38ffd30408e58ad5a7648484203
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-10-11 16:22:31 +0200

    Draft code for maing tabby subdataset lists
    I realized that adding super-subdataset relations is needed not only
    when loading tabby, but also whenever superdataset state progresses
    for reasons independent of tabby records. A standalone script is
    needed.

---
+++ load_subdatasets.py
@@ -0,0 +1,101 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from pprint import pprint
+import uuid
+
+from datalad_catalog.schema_utils import get_metadata_item
+from datalad_next.datasets import Dataset
+from datalad_tabby.io import load_tabby
+from pyld import jsonld
+
+parser = ArgumentParser()
+parser.add_argument("ds_path", type=Path)
+parser.add_argument("--tabby-anywhere", action="store_true")
+args = parser.parse_args()
+
+
+def get_tabby_subdataset_path(tabby_file_path, ds_root_path):
+    """Get path of subdataset described by tabby
+
+    Note: this is currently tuned to a single dir layout, and reports
+    tabby file's parent dir as the location of described subdataset.
+    If the tabby collection is located in .datalad/tabby, reports
+    relative to that directory instead.
+
+    """
+    relpath = tabby_file_path.parent.relative_to(ds_root_path)
+    if relpath.match(".datalad/tabby/*"):
+        return relpath.relative_to(".datalad/tabby/")
+    return relpath
+
+
+def lowercase_1st(x):
+    """Return lowercase string, using first element if list is provided"""
+    if isinstance(x, list):
+        return x[0].lower()
+    else:
+        return x.lower()
+
+
+def mint_dataset_id(fmt, ns="datalad.org", **kwargs):
+    id = uuid.uuid5(
+        namespace=uuid.uuid5(uuid.NAMESPACE_DNS, ns), name=fmt.format(**kwargs)
+    )
+    return id
+
+
+def list_tabby_ds_files(ds, anywhere=False):
+    """List dataset*.tsv tabby files
+
+    By default, used glob to report .datalad/tabby contents. If
+    searching anywhere is requested, uses ls-tree instead.
+
+    """
+    if not anywhere:
+        return list(ds.pathobj.glob(".datalad/tabby/**/*dataset*.tsv"))
+    else:
+        return [
+            ds.pathobj.joinpath(p)
+            for p in ds.repo.call_git_items_(["ls-files", "*dataset@tby*.tsv"])
+        ]
+
+
+def subdataset_item(ds, tabby_path):
+    """Report subdataset path, id, version"""
+
+    # path is derived from tabby location
+    ds_path = get_tabby_subdataset_path(tabby_path, ds.pathobj)
+
+    # id & version are derived from tabby content
+    record = load_tabby(
+        tabby_path,
+        cpaths=[Path(__file__).parent / "conventions"],
+    )
+
+    ds_id = mint_dataset_id(
+        fmt="sfb151.{project}.{name}",
+        project=lowercase_1st(record["crc-project"]),
+        name=record["name"],
+    )
+    ds_version = record["version"]
+
+    return {"dataset_path": ds_path, "dataset_id": ds_id, "version": ds_version}
+
+
+ds = Dataset(args.ds_path)
+
+dataset_item = get_metadata_item(
+    item_type='dataset',
+    dataset_id=ds.id,
+    dataset_version=ds.repo.get_hexsha(),
+    source_name="tabby",
+    source_version="0.1.0",
+)
+
+subdatasets = []
+for tabby_path in list_tabby_ds_files(ds, anywhere=args.tabby_anywhere):
+    subdatasets.append(subdataset_item(ds, tabby_path))
+
+dataset_item["subdatasets"] = subdatasets
+
+pprint(dataset_item)
